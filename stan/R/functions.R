3fa2e58cff836fdbbf0e6446ec06b6b834080769
wlandau <will.landau@lilly.com>
2021-06-14 12:23:42 -0400

    Improve model

--- stan/R/functions.R
+++ stan/R/functions.R
@@ -1,13 +1,21 @@
 simulate_data <- function(n = 10L) {
-  alpha <- rnorm(n = 1, mean = 0, sd = 1)
-  beta <- rnorm(n = n, mean = 0, sd = 1)
+  beta <- rnorm(n = 2, mean = 0, sd = 1)
   x <- seq(from = -1, to = 1, length.out = n)
-  y <- rnorm(n, alpha + x * beta, 1)
+  y <- rnorm(n, beta[1] + x * beta[2], 1)
   list(
     n = n,
     x = x,
     y = y,
-    # Hold on to prior draws:
-    .join_data = list(alpha = alpha, beta = beta)
+    # Hold on to draws from the prior:
+    .join_data = list(beta = beta)
   )
+}
+
+calculate_coverage <- function(results) {
+  results %>%
+    group_by(variable) %>%
+    summarize(
+      coverage_50 = mean(q25 < .join_data & .join_data < q75),
+      coverage_95 = mean(q2.5 < .join_data & .join_data < q97.5)
+    )
 }
--- stan/_targets.R
+++ stan/_targets.R
@@ -2,24 +2,15 @@ library(targets)
 library(stantargets)
 source("R/functions.R")
 tar_option_set(packages = "tidyverse")
-options(clustermq.scheduler = "multicore")
+options(clustermq.scheduler = "multicore") #<<
 list(
   tar_stan_mcmc_rep_summary(
-    model,
-    "model.stan",
-    simulate_data(),
-    batches = 80,
-    reps = 25,
-    variables = c("alpha", "beta"),
+    name = model, stan_files = "model.stan", data = simulate_data(),
+    batches = 160, reps = 25, variables = "beta",
     summaries = list(
-      ~posterior::quantile2(.x, probs = c(0.025, 0.975)),
+      ~posterior::quantile2(.x, probs = c(0.025, 0.25, 0.75, 0.975)),
       rhat = posterior::rhat
-    )
+    ),
   ),
-  tar_target(
-    coverage,
-    model %>%
-      group_by(variable) %>%
-      summarize(rate = mean(q2.5 < .join_data & .join_data < q97.5))
-  )
+  tar_target(coverage, calculate_coverage(model))
 )
